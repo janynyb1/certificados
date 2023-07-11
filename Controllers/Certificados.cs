4e5e11922b878eda5ae4ce9c684204ab0aae71eb
Janyny Barbosa <128196138+janynyb1@users.noreply.github.com>
2023-07-11 17:30:37 -0300

    Add files via upload

---
+++ Controllers/Certificados.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContatos.Controllers
+{
+    public class Certificados : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    public IActionResult Criar()
+    {
+        return View();
+    }
+    public IActionResult Editar()
+    {
+        return View();
+    }
+    public IActionResult Apagar()
+        {
+         return View();
+        }
+    }
+}
+
+
+
---
+++ Views/Contato/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ControleDeContatos.Views.Contato
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
